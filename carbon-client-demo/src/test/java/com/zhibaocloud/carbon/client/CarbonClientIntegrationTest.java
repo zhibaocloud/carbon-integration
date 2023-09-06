1598147b1cd0780e59ea02552d994582ab1402d1
Lu, Jun <lujun@wisefin.tech>
2023-09-06 12:36:48 +0800

    chore: add test debug log

--- carbon-client-demo/src/test/java/com/zhibaocloud/carbon/client/CarbonClientIntegrationTest.java
+++ carbon-client-demo/src/test/java/com/zhibaocloud/carbon/client/CarbonClientIntegrationTest.java
@@ -22,9 +22,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @Import(MockHttpConfiguration.class)
+@ActiveProfiles("test")
 class CarbonClientIntegrationTest {
 
   @Autowired
