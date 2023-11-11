d0ebf87ce6e0a680633c09fe881a0ef0cae01f27
mawenhao <mawh@bxlsj.com>
2023-11-12 01:32:46 +0800

    chore: add missing bean condition to serializer factory configuration

--- carbon-serializer-spring-boot-starter/src/main/java/com/zhibaocloud/carbon/intg/serialier/starter/CarbonSerializerAutoConfiguration.java
+++ carbon-serializer-spring-boot-starter/src/main/java/com/zhibaocloud/carbon/intg/serialier/starter/CarbonSerializerAutoConfiguration.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zhibaocloud.carbon.intg.jackson.CarbonJacksonSerializerFactory;
 import com.zhibaocloud.carbon.intg.serializer.CarbonSerializerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -13,6 +14,7 @@ public class CarbonSerializerAutoConfiguration {
 
   @Bean
   @ConditionalOnClass(ObjectMapper.class)
+  @ConditionalOnMissingBean(CarbonSerializerFactory.class)
   public CarbonSerializerFactory mapperFactory() {
     return new CarbonJacksonSerializerFactory();
   }
