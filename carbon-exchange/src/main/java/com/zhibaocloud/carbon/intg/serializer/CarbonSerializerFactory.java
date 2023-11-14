10de6ddff4ebaa4604f57d5f9c350aabaa09aac7
Lu, Jun <34977084+lujun2@users.noreply.github.com>
2023-11-14 10:04:48 +0800

    feat: 支持使用 SPI 加载组件 (#46)

--- carbon-exchange/src/main/java/com/zhibaocloud/carbon/intg/serializer/CarbonSerializerFactory.java
+++ carbon-exchange/src/main/java/com/zhibaocloud/carbon/intg/serializer/CarbonSerializerFactory.java
@@ -1,9 +1,17 @@
 package com.zhibaocloud.carbon.intg.serializer;
 
+import java.util.ServiceLoader;
+
 /**
  * @author yangtuo
  */
 public interface CarbonSerializerFactory {
 
   CarbonSerializer create(SerializationConfiguration config);
+
+  static CarbonSerializerFactory newInstance() {
+    ServiceLoader<CarbonSerializerFactory> loader =
+        ServiceLoader.load(CarbonSerializerFactory.class);
+    return loader.iterator().next();
+  }
 }
