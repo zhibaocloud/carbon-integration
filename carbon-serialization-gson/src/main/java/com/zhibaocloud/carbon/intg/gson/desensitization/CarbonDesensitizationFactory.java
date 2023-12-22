c8e3d97edba88946a2c21a04eb132d15d7c7f725
mawenhao <mawh@bxlsj.com>
2023-12-22 16:14:11 +0800

    chore: setup copyright

--- carbon-serialization-gson/src/main/java/com/zhibaocloud/carbon/intg/gson/desensitization/CarbonDesensitizationFactory.java
+++ carbon-serialization-gson/src/main/java/com/zhibaocloud/carbon/intg/gson/desensitization/CarbonDesensitizationFactory.java
@@ -1,5 +1,17 @@
-package com.zhibaocloud.carbon.intg.gson.desensitization;
+/*
+ * Copyright (c) 2018-2023. Chengdu WeiSiFan Technology Co., Ltd.
+ * Carbon Integration SDK is licensed under Mulan PSL v2.
+ *
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
 
+package com.zhibaocloud.carbon.intg.gson.desensitization;
 
 import com.zhibaocloud.carbon.intg.desensitization.CarbonDesensitization;
 import java.util.HashMap;
@@ -16,7 +28,8 @@ public class CarbonDesensitizationFactory {
   private static final Map<Class<? extends CarbonDesensitization<?>>, CarbonDesensitization<?>> map = new HashMap<>();
 
   @SuppressWarnings("all")
-  public static CarbonDesensitization<?> getDesensitization(Class<? extends CarbonDesensitization<?>> clazz) {
+  public static CarbonDesensitization<?> getDesensitization(
+      Class<? extends CarbonDesensitization<?>> clazz) {
     return map.computeIfAbsent(clazz, k -> {
       try {
         return (CarbonDesensitization<?>) k.newInstance();
